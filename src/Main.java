5b6c52a852e959a7806f8ef841787ba2d4c8899b
Semenov_Atryom <arsem.personal@gmail.com>
2022-02-21 13:11:45 +0300

    Program is created

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String first_name, last_name;
+        first_name = "Leonid";
+        last_name = "Andreev";
+
+        PrintName.printBoth(first_name,last_name);
+    }
+}
---
+++ src/PrintName.java
@@ -0,0 +1,5 @@
+public class PrintName{
+    public static void printBoth(String firstName, String lastName) {
+        System.out.println(firstName + " " + lastName);
+    }
+}
